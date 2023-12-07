2f6dc4d319dd93a5254d957b7c26f364fb4660d3
chandan1971 <chandandey1357@gmail.com>
2023-12-07 20:25:07 +0530

    Adding todo Acheived

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
+import Todo from "./components/Todo";
+import { addTodo, getAllTodo } from "./utils/handleApi";
 
 function App() {
+
+  const [todo,setTodo]=useState([])
+  const [text,setText]=useState("")
+
+  useEffect(()=>{
+    getAllTodo(setTodo)
+  },[])
   return (
     <div className="App">
       <div className="container">
-        <h1>ToDo App</h1>
+        <h1>WhatToDo App</h1>
         <div className="top">
-          <input type="text" placeholder="Add Todos..."/>
-          <div className="add">Add</div>
+          <input type="text" 
+          placeholder="Add Todos..."
+          value={text}
+          onChange={(event)=>{
+            setText(event.target.value)
+          }}
+          />
+          <div className="add" onClick={()=>addTodo(text,setText,setTodo)}>Add</div>
         </div>
-        
+        <div className="list">
+          {todo.map((item)=> <Todo key={item._id} text={item.text}></Todo>)}
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/utils/handleApi.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+const baseURL="http://localhost:8000"
+
+const getAllTodo=(setTodo)=>{
+    axios
+    .get(baseURL)
+    .then(({data})=>{
+        console.log(`data---->${data}`);
+        setTodo(data)
+    })
+}
+
+const addTodo=(text,setText,setTodo)=>{
+    axios
+    .post(`${baseURL}/save`,{text})
+    .then((data)=>{
+        console.log(data);
+        setText("")
+        getAllTodo(setTodo)
+    })
+}
+
+
+export {getAllTodo,addTodo}
